8b674c9d5bc304db3ed431a6fffaefd2ef11e82a
William Galindez Arias <william.dev.env@gmail.com>
2019-09-13 16:56:28 +0200

    Add files via upload

---
+++ face/chat.js
@@ -0,0 +1,386 @@
+var KEY_ENTER=13;
+$(document).ready(function(){
+	var $input=$(".chat-input")
+		,$sendButton=$(".chat-send")
+		,$messagesContainer=$(".chat-messages")
+		,$messagesList=$(".chat-messages-list")
+		,$effectContainer=$(".chat-effect-container")
+		,$infoContainer=$(".chat-info-container")
+
+		,messages=0
+		,bleeding=100
+		,isFriendTyping=false
+		,incomingMessages=0
+		,lastMessage=""
+	;
+	
+	
+
+	function gooOn(){
+		setFilter('url(#goo)');
+	}
+	function gooOff(){
+		setFilter('none');
+	}
+	function setFilter(value){
+		$effectContainer.css({
+			webkitFilter:value,
+			mozFilter:value,
+			filter:value,
+		});
+	}
+
+	function addMessage(message,self){
+		var $messageContainer=$("<li/>")
+			.addClass('chat-message '+(self?'chat-message-self':'chat-message-friend'))
+			.appendTo($messagesList)
+		;
+		var $messageBubble=$("<div/>")
+			.addClass('chat-message-bubble')
+			.appendTo($messageContainer)
+		;
+		$messageBubble.text(message);
+
+		var oldScroll=$messagesContainer.scrollTop();
+		$messagesContainer.scrollTop(9999999);
+		var newScroll=$messagesContainer.scrollTop();
+		var scrollDiff=newScroll-oldScroll;
+		TweenMax.fromTo(
+			$messagesList,0.4,{
+				y:scrollDiff
+			},{
+				y:0,
+				ease:Quint.easeOut
+			}
+		);
+
+		return {
+			$container:$messageContainer,
+			$bubble:$messageBubble
+		};
+	}
+	function sendMessage(){
+		var message=$input.text();
+		
+		if(message=="") return;
+
+				 
+				var contenido = message.includes("chucha");
+	   if(contenido === true){
+		   
+				
+				
+			$(".chat-messages").css("background-image", "url(img/chucha.png)");
+			setTimeout(function(){ $(".chat-messages").css("background-image", "url(img/fondo.png)"); },3000);
+
+	   }
+	   
+	$('ul.infosugerencias').empty();
+
+
+	
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:5005/webhooks/rest/webhook', //Acá pone la direccion a donde manda el msj q el usuario escribe
+		//url: 'http://localhost:5000/parser',
+		dataType: 'json',
+		contentType: 'application/json',
+		data: JSON.stringify( { "sender": "00001", "message": message } ),
+        success: function(respuesta) {
+			
+				/*
+				El formato para q mande los datos en json es:
+				 json_encode(array(
+							'mensaje' => 'ACÁ MANDA EL MENSAJE',
+							'sugerenciap' => 'ACÁ MANDA LA PRIMERA SUGERENCIA',
+							'sugerencias' => 'ACÁ MANDA LA SEGUNDA SUGERENCIA',
+							'sugerenciat' => 'ACÁ MANDA LA TERCERA SUGERENCIA',
+							
+						));
+				*/
+				if(respuesta !== ""){
+					respuestatxt = JSON.parse(JSON.stringify(respuesta));
+					
+					//var contenido = respuestatxt.mensaje.includes("chucha");
+					/*
+					var contenido = respuestatxt.message.includes("chucha");
+					if(contenido==true){
+						$(".chat-messages").css("background-image", "url(img/chucha.png)");
+						setTimeout(function(){ $(".chat-messages").css("background-image", "url(img/fondo.png)"); },3000);			
+					}
+					*/
+					//$("ol"). append("<li class=\"chat-message chat-message-friend\"><div class=\"chat-message-bubble\">"+respuestatxt.mensaje+"</div></li>");
+					$("ol"). append("<li class=\"chat-message chat-message-friend\"><div class=\"chat-message-bubble\">"+respuestatxt[0].text+"</div></li>");
+					
+					if(respuestatxt.sugerenciap!="" || respuestatxt.sugerencias!="" ||  respuestatxt.sugerenciat!="" ){
+						$('ul.infosugerencias').empty()
+						if(respuestatxt.sugerenciap!=""){
+							$("ul"). append("<li >"+respuestatxt.sugerenciap+"</li>");
+						}
+						if(respuestatxt.sugerencias!=""){
+							$("ul"). append("<li >"+respuestatxt.sugerencias+"</li>");
+						}
+						if(respuestatxt.sugerenciat!=""){
+							$("ul"). append("<li >"+respuestatxt.sugerenciat+"</li>");
+						}
+					}
+				}
+	
+			
+        }
+    });
+	
+
+		lastMessage=message;
+
+		var messageElements=addMessage(message,true)
+			,$messageContainer=messageElements.$container
+			,$messageBubble=messageElements.$bubble
+		;
+
+		var oldInputHeight=$(".chat-input-bar").height();
+		$input.text('');
+		updateChatHeight();
+		var newInputHeight=$(".chat-input-bar").height();
+		var inputHeightDiff=newInputHeight-oldInputHeight
+
+		var $messageEffect=$("<div/>")
+			.addClass('chat-message-effect')
+			.append($messageBubble.clone())
+			.appendTo($effectContainer)
+			.css({
+				left:$input.position().left-12,
+				top:$input.position().top+bleeding+inputHeightDiff
+			})
+		;
+
+
+		var messagePos=$messageBubble.offset();
+		var effectPos=$messageEffect.offset();
+		var pos={
+			x:messagePos.left-effectPos.left,
+			y:messagePos.top-effectPos.top
+		}
+
+		var $sendIcon=$sendButton.children("i");
+		TweenMax.to(
+			$sendIcon,0.15,{
+				x:30,
+				y:-30,
+				force3D:true,
+				ease:Quad.easeOut,
+				onComplete:function(){
+					TweenMax.fromTo(
+						$sendIcon,0.15,{
+							x:-30,
+							y:30
+						},
+						{
+							x:0,
+							y:0,
+							force3D:true,
+							ease:Quad.easeOut
+						}
+					);
+				}
+			}
+		);
+
+		gooOn();
+
+		
+		TweenMax.from(
+			$messageBubble,0.8,{
+				y:-pos.y,
+				ease:Sine.easeInOut,
+				force3D:true
+			}
+		);
+
+		var startingScroll=$messagesContainer.scrollTop();
+		var curScrollDiff=0;
+		var effectYTransition;
+		var setEffectYTransition=function(dest,dur,ease){
+			return TweenMax.to(
+				$messageEffect,dur,{
+					y:dest,
+					ease:ease,
+					force3D:true,
+					onUpdate:function(){
+						var curScroll=$messagesContainer.scrollTop();
+						var scrollDiff=curScroll-startingScroll;
+						if(scrollDiff>0){
+							curScrollDiff+=scrollDiff;
+							startingScroll=curScroll;
+
+							var time=effectYTransition.time();
+							effectYTransition.kill();
+							effectYTransition=setEffectYTransition(pos.y-curScrollDiff,0.8-time,Sine.easeOut);
+						}
+					}
+				}
+			);
+		}
+
+		effectYTransition=setEffectYTransition(pos.y,0.8,Sine.easeInOut);
+		
+		// effectYTransition.updateTo({y:800});
+
+		TweenMax.from(
+			$messageBubble,0.6,{
+				delay:0.2,
+				x:-pos.x,
+				ease:Quad.easeInOut,
+				force3D:true
+			}
+		);
+		TweenMax.to(
+			$messageEffect,0.6,{
+				delay:0.2,
+				x:pos.x,
+				ease:Quad.easeInOut,
+				force3D:true
+			}
+		);
+
+		TweenMax.from(
+			$messageBubble,0.2,{
+				delay:0.65,
+				opacity:0,
+				ease:Quad.easeInOut,
+				onComplete:function(){
+					TweenMax.killTweensOf($messageEffect);
+					$messageEffect.remove();
+					if(!isFriendTyping)
+						gooOff();
+				}
+			}
+		);
+
+
+	}
+	
+	function friendIsTyping(){
+		if(isFriendTyping) return;
+
+		isFriendTyping=true;
+
+		var $dots=$("<div/>")
+			.addClass('chat-effect-dots')
+			.css({
+				top:-30+bleeding,
+				left:10
+			})
+			.appendTo($effectContainer)
+		;
+		for (var i = 0; i < 3; i++) {
+			var $dot=$("<div/>")
+				.addClass("chat-effect-dot")
+				.css({
+					left:i*20
+				})
+				.appendTo($dots)
+			;
+			TweenMax.to($dot,0.3,{
+				delay:-i*0.1,
+				y:30,
+				yoyo:true,
+				repeat:-1,
+				ease:Quad.easeInOut
+			})
+		};
+
+		var $info=$("<div/>")
+			.addClass("chat-info-typing")
+			.text("Your friend is typing...")
+			.css({
+				transform:"translate3d(0,30px,0)"
+			})
+			.appendTo($infoContainer)
+
+		TweenMax.to($info, 0.3,{
+			y:0,
+			force3D:true
+		});
+
+		gooOn();
+	}
+
+	function friendStoppedTyping(){
+		if(!isFriendTyping) return
+
+		isFriendTyping=false;
+
+		var $dots=$effectContainer.find(".chat-effect-dots");
+		TweenMax.to($dots,0.3,{
+			y:40,
+			force3D:true,
+			ease:Quad.easeIn,
+		});
+
+		var $info=$infoContainer.find(".chat-info-typing");
+		TweenMax.to($info,0.3,{
+			y:30,
+			force3D:true,
+			ease:Quad.easeIn,
+			onComplete:function(){
+				$dots.remove();
+				$info.remove();
+
+				gooOff();
+			}
+		});
+	}
+	function receiveMessage(message){
+		var messageElements=addMessage(message,false)
+			,$messageContainer=messageElements.$container
+			,$messageBubble=messageElements.$bubble
+		;
+
+		TweenMax.set($messageBubble,{
+			transformOrigin:"60px 50%"
+		})
+		TweenMax.from($messageBubble,0.4,{
+			scale:0,
+			force3D:true,
+			ease:Back.easeOut
+		})
+		TweenMax.from($messageBubble,0.4,{
+			x:-100,
+			force3D:true,
+			ease:Quint.easeOut
+		})
+	}
+
+	function updateChatHeight(){
+		$messagesContainer.css({
+			height:460-$(".chat-input-bar").height()
+		});
+	}
+
+	$input.keydown(function(event) {
+		if(event.keyCode==KEY_ENTER){
+			event.preventDefault();
+			sendMessage();
+		}
+	});
+	$sendButton.click(function(event){
+		event.preventDefault();
+		sendMessage();
+		// $input.focus();
+	});
+	$sendButton.on("touchstart",function(event){
+		event.preventDefault();
+		sendMessage();
+		// $input.focus();
+	});
+
+	$input.on("input",function(){
+		updateChatHeight();
+	});
+
+	gooOff();
+	updateChatHeight();
+
+
+})
---
+++ face/index.php
@@ -0,0 +1,360 @@
+<?php header('Access-Control-Allow-Origin: *');?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+		<title>chat</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
+<link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<style>
+*{
+
+font-family: 'Oswald', sans-serif;
+}
+.content {
+    position: relative;
+}
+
+.chat-window button:focus {
+	-webkit-tap-highlight-color: rgba(255,255,255,0);
+	-webkit-tap-highlight-color: transparent;
+	outline: none;
+}
+
+.chat-window {
+	width: 280px;
+	margin: 0 auto;
+	overflow: hidden;
+	border: 4px solid #000066;
+	color: #474c57;
+	border-radius: 20px;
+
+}
+
+
+
+.chat-window a {
+	color: #000066;
+}
+
+.chat-window a:hover,
+.chat-window a:focus {
+	color: #003399;
+}
+
+.chat-messages {
+	height: 350px;
+	overflow-x: hidden;
+	overflow-y: auto;
+	width: 100%;
+	position: relative;
+	border-radius: 2px 2px 0 0;
+	background: #fff;
+}
+
+.chat-messages-list {
+	list-style-type: none;
+	padding: 0;
+	margin: 0;
+	width: 100%;
+	padding: 20px 0 20px 0;
+}
+
+.chat-message {
+	position: relative;
+	font-size: 0;
+	margin-bottom: 10px;	
+    width: 90%;
+    margin: 10px 5% 10px 5%;
+}
+
+.chat-message-effect {
+	position: absolute;
+}
+
+.chat-message-bubble {
+	display: inline-block;
+	font-size: 14px;
+	max-width: 350px;
+	background: #E6E8F4;
+	padding: 8px 14px;
+	border-radius: 8px;
+	min-width: 0;
+}
+
+.chat-message-self.chat-message-merge-start .chat-message-bubble {
+	border-bottom-right-radius: 0;
+}
+
+.chat-message-self.chat-message-merge-middle .chat-message-bubble {
+	border-top-right-radius: 0;
+	border-bottom-right-radius: 0;
+}
+
+.chat-message-self.chat-message-merge-end .chat-message-bubble {
+	border-top-right-radius: 0;
+}
+
+.chat-message-self {
+	text-align: right;
+}
+
+.chat-message-self .chat-message-bubble,
+.chat-message-effect .chat-message-bubble { 
+	background: #e8e8e8;
+	text-align: left;
+}
+
+.chat-input-bar {
+	position: relative;
+	background:#200348;
+}
+
+.chat-input-wrapper {
+	position: relative;
+	z-index: 2;
+	background: #200348;
+	padding: 0.5em 0;
+	border-radius: 0 0 2px 2px;
+	color: #fff;
+}
+
+.chat-input-wrapper,
+.chat-send {
+	display: -webkit-flex;
+	display: -ms-flexbox;
+	display: flex;
+	-webkit-flex-wrap: wrap;
+	-ms-flex-wrap: wrap;
+	flex-wrap: wrap;
+	-webkit-justify-content: center;
+	justify-content: center;
+	font-size: 16px;
+}
+
+.chat-input-tool {
+	background: transparent;
+	border: none;
+	padding: 0 0.75em;
+	color: inherit;
+}
+
+.chat-input {
+	outline: none;
+	resize: none;
+	overflow: hidden;
+	min-height: 38px;
+	-webkit-flex: 1;
+	flex: 1;
+	font-size: 14px;
+	padding: 10px 0 7px;
+	cursor: text;
+	
+    width: 250px;
+    margin: 0 0 0 20px;
+	
+    border: 1px solid rgba(167, 214, 239, 0.5);
+    border-radius: 6px;
+}
+
+.chat-input:empty::before {
+	content: "Start typing...";
+	color: #ececec;
+}
+
+.chat-input:focus::before {
+	content: "";
+}
+
+.chat-send {
+	background: transparent;
+	border: none;
+	position: relative;
+	overflow: hidden;
+	padding: 0 0.75em;
+	color: inherit;
+	-webkit-transition: color 0.6s;
+	transition: color 0.6s;
+}
+
+.chat-input:empty + .chat-send {
+	color: #a7d6ef;
+}
+
+.chat-send>i {
+	position: relative;
+}
+
+.chat-effect-container {
+	position: absolute;
+	top: -100px;
+	width: 100%;
+}
+
+.chat-effect-bar {
+	background: #200348;
+	position: absolute;
+	top: 100px;
+	width: 100%;
+	height: 40px;
+	-webkit-transform: rotateY(0);
+	transform: rotateY(0);
+}
+
+.chat-effect-dots {
+	position: absolute;
+}
+
+.chat-effect-dot {
+	background: #200348;
+	position: absolute;
+	width: 15px;
+	height: 15px;
+	border-radius: 100%;
+}
+
+.chat-info-container {
+	position: absolute;
+	top: -20px;
+	font-size: 12px;
+	color: #2B8EC2;
+}
+
+.chat-info-typing {
+	position: absolute;
+	left: 80px;
+	white-space: nowrap;
+}
+.botonmas {
+    font-style: normal;
+    font-size: 24px;
+    font-weight: bold;
+	cursor:pointer;
+	text-align:center
+}
+
+.caja_opciones{
+    position: absolute;
+    width: 280px;
+    z-index: 1000000;
+	bottom:10px;
+	display:none;
+}
+
+.caja_opciones ul {
+	list-style:none;
+	margin:0;
+	padding:0;
+    background: #eee;
+    border-radius: 15px;
+}
+
+.caja_opciones ul li{
+    padding: 8px ;
+	border-bottom:1px solid #FFF;
+	cursor:pointer;
+}
+
+.caja_opciones ul li:last-child{
+	border-bottom:0px
+}
+
+.caja_opciones ul li i{
+	margin:0 10px 0 0;
+}
+
+.caja_opciones .opc_cancelar{
+	background: #eee;
+    text-align: center;
+    border-radius: 10px;
+    padding: 12px 0;
+    margin: 5px 0 0 0;
+	cursor:pointer;
+}
+.cajasugerencias {
+    min-height: 40px;
+    width: 100%;
+    display: block;
+}
+.cajasugerencias ul{
+	list-style:none;
+    padding: 0;
+}
+.cajasugerencias ul li{
+    display: inline-block;
+    text-align: center;
+    width: 32%;
+    color: #FFF;
+    font-size: 14px;
+	vertical-align:middle;
+}
+</style>
+			<div class="content">
+				<div class="chat-window">
+                	<div class="caja_opciones">
+                    	<ul>
+                        	<li><i class="fa fa-camera"></i>Camera</li>
+                        	<li><i class="fa fa-image"></i>Photo</li>
+                        	<li><i class="fa fa-file-text"></i>Context</li>
+                        	<li><i class="fa fa-location-arrow"></i>Location</li>
+                        	<li><i class="fa fa-pencil"></i>Language</li>
+                        </ul>
+                        <div class="opc_cancelar">Cancel</div>
+                    </div>
+					<div class="chat-messages">
+						<ol class="chat-messages-list">
+							
+						</ol>
+					</div>
+					<div class="chat-input-bar">
+						<div class="chat-info-container">
+
+						</div>
+						<div class="chat-effect-container">
+							<div class="chat-effect-bar"></div>
+						</div>
+						<div class="chat-input-wrapper">
+							<div class="botonmas">+</div>
+							<div class="chat-input" contenteditable></div>
+							<button class="chat-send">
+								<i class="fa fa-paper-plane"></i>
+							</button>
+                            <div class="cajasugerencias">
+                            	<ul class="infosugerencias">
+                                </ul>
+                            </div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="800">
+			  <defs>
+			    <filter id="goo">
+			      <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+			      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
+			      <feComposite in="SourceGraphic" in2="goo" />
+			    </filter>
+			  </defs>
+			</svg>
+			
+		</div><!-- /container -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/TweenMax.min.js"></script>
+		<script src="js/chat.js"></script>
+        <script>
+$(".opc_cancelar").click(function(){
+  $(".caja_opciones").hide();
+});		
+$(".botonmas").click(function(){
+  $(".caja_opciones").show();
+});		
+		</script>
+        
+</body>
+</html>
